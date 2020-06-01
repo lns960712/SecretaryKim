28a69bc9759d2c0bf162d724c7b21ace712f2fe8
Indian966 <rlaalsrb4175@gmail.com>
2020-06-01 18:14:05 +0900

    Update MainListActivity.java
    Database 레퍼런스를 업데이트 했습니다.
    이제 timestamp를 이용하여 회의ID를 만들 수 있습니다. 현재 참가중인 회의는 users에서 확인 할 수 있습니다.

--- app/src/main/java/com/android/SecretaryKim/MainListActivity.java
+++ app/src/main/java/com/android/SecretaryKim/MainListActivity.java
@@ -2,6 +2,7 @@ package com.android.SecretaryKim;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +18,7 @@ import com.google.firebase.database.FirebaseDatabase;
     LoginActivity 에서 넘어온다
     회의 생성 버튼을 누르면 ConferenceInfoActivity 로 넘어간다
     참가하고 있는 회의 정보에 대해 Firebase 에서 정보를 받아 올 수 있어야 한다
+    사용자를 초대하는 기능이 필요하다
     프로필 사진, 참가하고 있는 회의 리스트,
     회의 생성 버튼이 필요하다
  */
@@ -27,15 +29,14 @@ public class MainListActivity extends AppCompatActivity {
     private ImageView imageView;
     private DatabaseReference mDatabase;
     private FirebaseAuth mauth;
+    private String ConfId = null; // 회의 ID
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_list);
 
         mauth=FirebaseAuth.getInstance();
-
         mDatabase = FirebaseDatabase.getInstance().getReference();
-
 
         makeButton = findViewById(R.id.makeConference);
         imageView = (ImageView) findViewById(R.id.imageView);
@@ -53,8 +54,18 @@ public class MainListActivity extends AppCompatActivity {
     }
 
     protected void restoreData () {
+        // 현재 시간을 나타내는 timestamp를 생성
+        Long tsLong = System.currentTimeMillis()/1000;
+        String timestamp = tsLong.toString();
+        System.out.println("this time is :" + timestamp);
+
+        // 파이어베이스에 데이터 저장
         FirebaseUser user=mauth.getCurrentUser();
-        mDatabase.child("conferences").child(user.getUid()+"timestamp").setValue("hello world");
+        ConfId = user.getUid() + "_" + timestamp; // 회의 ID
+        mDatabase.child("conferences").child(ConfId).setValue("hello world");
+        // 유저 정보에 참여하고 있는 회의 저장 setValue가 아닌 add인지 확인 필요
+        // DB상에서 리스트로 보일 필요 있음
+        mDatabase.child("users").child(user.getUid()).child("conference").setValue(ConfId);
 
     }
 
